025106ebe3c1f9fe6519361cf5be7e2bf1924114
Joris Roos <roos.jor@gmail.com>
2025-06-30 17:12:09 -0400

    script update

--- generate_imports.sh
+++ generate_imports.sh
@@ -8,13 +8,14 @@ MODULE_PREFIX="LeanSampleProblems"
 
 echo "/- This file is auto-generated. Any changes will be overwritten -/" >> "$OUTPUT_FILE"
 echo "" >> "$OUTPUT_FILE"
-echo "set_option linter.dupNamespace false" >> "$OUTPUT_FILE"
-echo "" >> "$OUTPUT_FILE"
 
 # Loop over .lean files and write import lines
 for filepath in "$LEAN_DIR"/*.lean; do
     filename=$(basename "$filepath" .lean)
     echo "import $MODULE_PREFIX.$filename" >> "$OUTPUT_FILE"
 done
+
+echo "" >> "$OUTPUT_FILE"
+echo "set_option linter.dupNamespace false" >> "$OUTPUT_FILE"
 
 echo "Import file created: $OUTPUT_FILE"
